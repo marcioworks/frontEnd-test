6454db6ced89b99d05a45717f64dc66347e761f8
silvamarcio <silva.marcio86@hotmail.com>
2019-08-21 23:38:11 -0300

    validacao do formulario com JS

--- main.js
+++ main.js
@@ -8,6 +8,8 @@
   const nameInput = document.getElementById('name');
   const emailInput = document.getElementById('email');
   const messageInput = document.getElementById('message');
+    
+    
 
   // "addEventListener" instrui o navegador a executar uma função sempre
   // que um evento específico acontece. Nesse caso, a função abaixo será
@@ -15,16 +17,36 @@
   contactForm.addEventListener('submit', function() {
     // A propriedade "value" contém o valor atual de um input ou textarea
     const nameValue = nameInput.value;
+    const campoEmail = emailInput.value;
+    const campoMenssage = messageInput.value;
 
     // A propriedade "length" contém o tamanho (em caracteres) de uma
     // uma string. Toda string contém essa propriedade.
     const nameLength = nameValue.length;
 
+    if(nameValue == "" ){
+      alert("Campo Nome Vazio, digite Seu Nome");
+     
+    }
+    if(campoEmail == "" ){
+      alert("Campo Email Vazio, Digite seu email");
+      
+    }
+    if(campoEmail.indexOf('@') == -1 || campoEmail.indexOf('.') == -1 ){
+      alert("Insira um Email valido");
+    }
+    if(campoMenssage == ""){
+      alert("Campo Message Vazio, gostariamos de ouvir Oque Você tem A Dizer!");
+      
+    }
+    else{
+      window.alert(`Formulario enviado com Sucesso!`);
+    }
     // "window" é um objeto global que representa a janela (ou aba) do
     // navegador que está executando o código do seu site. O método
     // "alert" simplesmente mostra um aviso para o usuário contendo a
     // mensagem provida.
-    window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
+
 
     // Altere e complete essa função para validar os campos do formulário
     // de acordo com as especificações do teste. Boa sorte!
